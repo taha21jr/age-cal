2d22e65e37988f03f6154edf2288797476a74f17
taha21jr <taha.husnain20@gmail.com>
2023-06-23 23:47:55 +0500

    modified input fiedls

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,27 +1,56 @@
 import { useEffect, useState } from "react";
 import Input from "./Input";
 
+let ageYears = null,
+  ageMonths = null,
+  ageDays = null;
+
 const Form = () => {
   const [day, setDay] = useState("");
   const [months, setMonth] = useState("");
   const [year, setYear] = useState("");
   const [finalState, setFinalState] = useState({});
-
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const [dash, setDash] = useState(false);
 
   const handleSubmit = (event) => {
-    console.log(day);
+    console.log("day" + day, months, year);
+
     event.preventDefault();
-    setDash(true);
+    if (day === "" || months === "" || year === "") {
+      setIsSubmitted(true);
+    } else {
+      setIsSubmitted(false);
+      let currentDate = new Date();
+      let birthDate = new Date(year, months - 1, day);
+      let ageInMilliseconds = currentDate - birthDate;
+
+      let seconds = ageInMilliseconds / 1000;
+      let minutes = seconds / 60;
+      let hours = minutes / 60;
+      let days = hours / 24;
+
+      ageYears = Math.floor(days / 365);
+      ageMonths = Math.floor((days % 365) / 30.44);
+      ageDays = Math.floor((days % 365) % 30.44);
+
+      console.log("if", ageDays);
+      if (ageDays === "") {
+        console.log("hell0");
+      }
+
+      setDash(true);
+    }
   };
 
   useEffect(() => {
     if (dash) {
       const obj = {
-        day,
-        months,
-        year,
+        ageDays,
+        ageMonths,
+        ageYears,
       };
+
       setFinalState(obj);
       setDay("");
       setMonth("");
@@ -29,23 +58,36 @@ const Form = () => {
     }
   }, [dash]);
 
+  console.log("dasjh========", dash);
+  const handleClick = () => {
+    console.log(dash);
+    setDash(false);
+  };
+
   return (
     <form className="form" autoComplete="off">
       <div className="container">
         <div className="para-div">
-          <p>DAY</p>
-          <p className="month"> MONTH</p>
-          <p>YEAR</p>
+          <p className={isSubmitted ? "day-highlight" : ""}>DAY</p>
+          <p className={isSubmitted ? "month-highlight" : "month"}>MONTH</p>
+          <p className={isSubmitted ? "year-highlight" : ""}>YEAR</p>
         </div>
 
         <Input
           type="text"
+          onClick={handleClick}
           day={day}
           months={months}
           year={year}
           setDay={setDay}
           setMonth={setMonth}
           setYear={setYear}
+          submit={isSubmitted}
+          style={{
+            borderColor: isSubmitted
+              ? "hsl(0, 100%, 67%)"
+              : "hsl(259, 100%, 65%)",
+          }}
         />
 
         <hr />
@@ -63,17 +105,17 @@ const Form = () => {
         </button>
 
         <div className="ageCal">
-          <h1 className="dash">{dash ? finalState.day : "_ _"}</h1>
+          <h1 className="dash">{dash ? finalState.ageYears : "_ _"}</h1>
           <h1>years</h1>
         </div>
 
         <div className="ageCal">
-          <h1 className="dash">{dash ? finalState.months : "_ _"}</h1>
+          <h1 className="dash">{dash ? finalState.ageMonths : "_ _"}</h1>
           <h1>months</h1>
         </div>
 
         <div className="ageCal">
-          <h1 className="dash">{dash ? finalState.year : "_ _"}</h1>
+          <h1 className="dash">{dash ? finalState.ageDays : "_ _"}</h1>
           <h1>days</h1>
         </div>
       </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,24 +1,49 @@
-import React, { useEffect, useState } from "react";
-import Form from "./Form";
-
 const Input = (props) => {
+  const {
+    setDay,
+    setMonth,
+    setYear,
+    day,
+    months,
+    year,
+    submit,
+    style,
+    onClick,
+  } = props;
+  console.log("in input comp", props);
 
-  const { setDay, setMonth, setYear ,day ,months,year } = props;
-  // console.log(day, months, year);
-  const handleInputChange = (setState)=>(event)=>{  
-    setState(event.target.value)
-
-  }
+  const handleInputChange = (setState) => (event) => {
+    setState(event.target.value);
+  };
 
   return (
-    <div className='input-fields'>
-        
-            <input  placeholder= "DD" value={day} onChange={handleInputChange(setDay)}/>
-            <input  placeholder= "MM" value={months} onChange={handleInputChange(setMonth)}/>
-            <input  placeholder= "YYYY" value={year} onChange={handleInputChange(setYear)}/>
-        
+    <div className="input-fields">
+      <input
+        placeholder="DD"
+        className={submit && "red-placeholder"}
+        value={day}
+        onChange={handleInputChange(setDay)}
+        style={style}
+        onClick={onClick}
+      />
+      <input
+        placeholder="MM"
+        className={submit && "red-placeholder"}
+        value={months}
+        onChange={handleInputChange(setMonth)}
+        style={style}
+        onClick={onClick}
+      />
+      <input
+        placeholder="YYYY"
+        className={submit && "red-placeholder"}
+        value={year}
+        onChange={handleInputChange(setYear)}
+        style={style}
+        onClick={onClick}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
